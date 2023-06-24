86a0ea7a1ec2eec2c0c2bb5a40a7b6e01fdb47d6
Pratap Rajput <95515165+prataprajput27@users.noreply.github.com>
2023-06-24 12:48:41 +0530

    solved issues of private route

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ import { setUser } from "./redux/features/authSlice";
 import AddEditTour from "./pages/AddEditTour";
 import SingleTour from "./pages/SingleTour";
 import Dashboard from "./pages/Dashboard";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   const dispatch = useDispatch();
@@ -28,10 +29,31 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
-          <Route path="/addTour" element={<AddEditTour />} />
-          <Route path="/editTour/:id" element={<AddEditTour />} />
+          <Route
+            path="/addTour"
+            element={
+              <PrivateRoute>
+                <AddEditTour />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/editTour/:id"
+            element={
+              <PrivateRoute>
+                <AddEditTour />
+              </PrivateRoute>
+            }
+          />
           <Route path="/tour/:id" element={<SingleTour />} />
-          <Route path="/dashboard" element={<Dashboard />} />
+          <Route
+            path="/dashboard"
+            element={
+              <PrivateRoute>
+                <AddEditTour />
+              </PrivateRoute>
+            }
+          />
         </Routes>
       </div>
     </BrowserRouter>
